da66b89954d77965fa6a32bfa1983dfd1146c005
Arne Wiese <arne@wiese.me>
2021-02-27 10:09:18 +0100

    initial commit

---
+++ src/components/Styles.svelte
@@ -0,0 +1,6 @@
+<style global>
+    :global(.form-input),
+    :global(.form-select) {
+      @apply block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm;
+    }
+</style>
